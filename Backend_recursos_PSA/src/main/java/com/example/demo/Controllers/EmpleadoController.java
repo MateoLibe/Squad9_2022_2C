fcaa3bcd0371a82e701de54f45e7959d03341925
lucas-dem <lucasd393939@gmail.com>
2022-11-11 16:55:42 -0300

    Idea para empleados, dudas sobre funciones put, falta swagger para estos?

--- Backend_recursos_PSA/src/main/java/com/example/demo/Controllers/EmpleadoController.java
+++ Backend_recursos_PSA/src/main/java/com/example/demo/Controllers/EmpleadoController.java
@@ -1,5 +1,43 @@
 package com.example.demo.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.Model.Empleado;
+import com.example.demo.Service.EmpleadoService;
+
+
 public class EmpleadoController {
-    
+    @Autowired
+    private EmpleadoService empleadoService;
+
+    @GetMapping("/empleado")
+    public Collection<Empleado> getEmpleado() throws Throwable{
+        return empleadoService.getEmpleados();
+    }
+
+    @GetMapping("/empleado/{legajo}")
+    public Empleado getSingleEmpleado(integrer legajo) throws Throwable{
+        return empleadoService.getEmpleadoByLegajo(legajo);
+    }
 }
--- Backend_recursos_PSA/src/main/java/com/example/demo/Service/EmpleadoService.java
+++ Backend_recursos_PSA/src/main/java/com/example/demo/Service/EmpleadoService.java
@@ -1,7 +1,31 @@
 package com.example.demo.Service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.Model.CargaHoras;
+import com.example.demo.Repository.CargaHorasRepository;
+
+import org.springframework.web.client.RestTemplate;
+import java.util.*;
 
 public class EmpleadoService {
-    
+
+    public Collection<Empleado> getEmpleados() throws Throwable{
+        String uri = "" //falta el identificador de recursos uniforme no se de donde sacarlo?
+        RestTempate restTemplate = new RestTemplate();
+        Empleado[] empleados = restTemplate.getForObject(uri, Empleado[].class);
+        return Array.asList(empleados);
+    }
+
+    public Empleado getEmpleadoByLegajo(integrer legajo) throws Throwable{
+        ArrayList<Empleado> empleados = new ArrayList<>(getEmpleados());
+
+        Empleado empleadoBuscado = empleados.stream().filter(empleado -> Objetcs.equals(legajo, empleado.getLegajo()))
+                .findAny().orElse(null);
+        return empleadoBuscado;
+    }
+
 }
